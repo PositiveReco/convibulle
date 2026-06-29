7df2fa1b366b59b2314f465a53328b0f04c17501
PositiveReco <positivereco.app@gmail.com>
2026-06-29 20:20:29 +0200

    mode paysage siege

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ConviBulle — service worker (Étape 1) : cache pour fonctionner hors-ligne */
-const CACHE = 'convibulle-v10';
+const CACHE = 'convibulle-v11';
 const ASSETS = [
   './',
   './index.html',
