83c0c9cb8576795570b906e5b858538b96dfa636
PositiveReco <positivereco.app@gmail.com>
2026-06-29 23:05:04 +0200

    Bouton quitter

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ConviBulle — service worker (Étape 1) : cache pour fonctionner hors-ligne */
-const CACHE = 'convibulle-v16';
+const CACHE = 'convibulle-v17';
 const ASSETS = [
   './',
   './index.html',
