c077d8155b0c44a97a75befd19963ed5dbe3fae1
PositiveReco <positivereco.app@gmail.com>
2026-07-29 13:56:35 +0200

    Reprise partie

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ConviBulle — service worker (Étape 1) : cache pour fonctionner hors-ligne */
-const CACHE = 'convibulle-v25';
+const CACHE = 'convibulle-v26';
 const ASSETS = [
   './',
   './index.html',
